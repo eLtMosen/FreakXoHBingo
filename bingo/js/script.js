bb7febe3c543717128ba14df754093e099dcbbff
Lukas Havemann <havemann.lukas@gmail.com>
2015-11-28 11:37:39 +0800

    Mischen Funktion neugeschrieben; lodash als zusätzliches Framework mit eingebunden

--- bingo/js/script.js
+++ bingo/js/script.js
@@ -1,30 +1,14 @@
 $(document).ready(function() {
   var config = {
-    map : {
+    bingoCard : {
       width : 7,
       height : 7,
       size : 49
     },
     buzzwordCount : 57
   };
-  console.log(config.map.size)
-  var benutzteKarten = new Array(config.buzzwordCount);
-  var geklicktArray = new Array(48);
+
   var gewinnArray = {
-    "Vertikal1": [0, 1, 2, 3, 4, 5, 6],
-    "Vertikal2": [7, 8, 9, 10, 11, 12, 13],
-    "Vertikal3": [14, 15, 16, 17, 18, 19, 20],
-    "Vertikal4": [21, 22, 23, 48, 24, 25, 26],
-    "Vertikal5": [27, 28, 29, 30, 31, 32, 33],
-    "Vertikal6": [34, 35, 36, 37, 38, 39, 40],
-    "Vertikal7": [41, 42, 43, 44, 45, 46, 47],
-    "Horizontal1": [0, 7, 14, 21, 27, 34, 41],
-    "Horizontal2": [1, 8, 15, 22, 28, 35, 42],
-    "Horizontal3": [2, 9, 16, 23, 29, 36, 43],
-    "Horizontal4": [3, 10, 17, 48, 30, 37, 44],
-    "Horizontal5": [4, 11, 18, 24, 31, 38, 45],
-    "Horizontal6": [5, 12, 19, 25, 32, 39, 46],
-    "Horizontal7": [6, 13, 20, 26, 33, 40, 47],
     "Diagonal1": [0, 8, 16, 48, 31, 39, 47],
     "Diagonal2": [6, 12, 18, 48, 29, 35, 41],
     "FreakshowF": [15, 16, 17, 18, 19, 22, 28, 30, 35, 48],
@@ -33,114 +17,86 @@ $(document).ready(function() {
     "Bierkrug": [8, 9, 10, 11, 12, 20, 26, 28, 29, 30, 31, 32, 35, 38, 43, 44],
     "Apfel": [3, 4, 9, 10, 11, 12, 16, 17, 18, 19, 20, 22, 23, 24, 25, 27, 29, 30, 31, 32, 33, 36, 39, 48]
   }
-  var nummer = 0;
 
-  createBingoBody();
   init();
 
-  function enthaeltAlle(nadeln, heuhaufen) {
-    for (var i = 0, len = nadeln.length; i < len; i++) {
-      if ($.inArray(nadeln[i], heuhaufen) == -1) return false;
-    }
-    return true;
-  }
-
   function init() {
-    for (var i = 1; i <= config.map.size; i++) {
-      karte_fuellen(i);
-    }
-
-    var l = 1;
-    for (var k = 1; k < config.buzzwordCount; k++) {
-      if (benutzteKarten[k] != true) {
-        $('#fehlt' + l).html('<img src="images/' + k + '.svg">');
-        l++;
-      }
-    }
+    createBingoCard();
+    bindEventHandler();
+    initBingoCard();
   }
 
-  function createBingoBody() {
+  function createBingoCard() {
     var $BingoBody = $('#BingoBody');
     var $row = $('<tr>');
     
-    for (var i = 1; i <= config.map.size; i++) {
-      $row.append($('<td>').attr('id' , 'cell' + i));
+    _.times(config.bingoCard.size, function(i) {
+      $cell = $('<td>');
+      $cell.attr('id' , 'cell' + i);
+      $cell.append($('<img>'));
 
-      if(i % config.map.width == 0) {
+      $row.append($cell);
+
+      // Ueberpruefe ob Reihe voll
+      if(i % config.bingoCard.width == config.bingoCard.width - 1) {
         $BingoBody.append($row);
-        var $row = $('<tr>');
-      }
-    }
-  }
-
-  function karte_fuellen(i) {
-    nummer = (Math.floor(Math.random() * config.buzzwordCount) + 1);
-
-    if (benutzteKarten[nummer] != true) {
-      $('#cell' + i).html('<img src="images/' + nummer + '.svg">');
-      benutzteKarten[nummer] = true;
-    } else {
-      karte_fuellen(i);
-    }
-  }
-
-  function benutzteKartenZuruecksetzen() {
-    for (var j = 0; j < benutzteKarten.length; j++) {
-      benutzteKarten[j] = false;
-    }
-  }
-
-
-  $('#neueKarte').click(function() {
-    resize(100, 750);
-    benutzteKartenZuruecksetzen();
-    init();
-  });
-
-  $('#klein').click(function() {
-    resize(75, 575);
-  });
-
-  $('#normal').click(function() {
-    resize(100, 750);
-  });
-
-  $('#mittel').click(function() {
-    resize(150, 1100);
-  });
-  
-  $('#gross').click(function(tileSize, fullSize) {
-    resize(200, 1450);
-  });
-
-  $('#riesig').click(function() {
-    resize(300, 2150);
-  });
-
-  function resize(tileSize, fullSize) {
-    $('div.resize img').width(tileSize).height(tileSize);
-    $('div.resize td').width(tileSize).height(tileSize);
-    $('div.resize').width(fullSize);
-  }
-
-  $("#BingoBody td").click(function() {
-    $(this).toggleClass("clickedCell");
-    geklicktArray[this.id.slice(5)] = parseInt(this.id.slice(5), 10);
-    var i = 1;
-    $.each(gewinnArray, function(key, value) {
-      if (enthaeltAlle(value, geklicktArray)) {
-        $('#result' + i).html(key);
-        $.each(gewinnArray, function(key, value) {
-          if (enthaeltAlle(value, geklicktArray)) {
-            for (var j = 0; j < value.length;) {
-              $('#cell' + value[j]).addClass("gruene_zelle");
-              j++;
-            }
-          }
-        });
-        i++;
+        $row = $('<tr>');
       }
     });
-    console.log($(location).attr('href'));
-  });
+  }
+
+  function initBingoCard() {
+    var allBingoCardIds = _.range(1, config.buzzwordCount);
+
+    _.times(config.bingoCard.size, function(i) {
+      var randomNum = _.random(1, allBingoCardIds.length - 1);
+      var id = allBingoCardIds[randomNum];
+      $('#cell' + i).find('img').attr('src', 'images/' + id + '.svg');
+
+      allBingoCardIds = _.without(allBingoCardIds, id);
+    });
+
+    _.times(allBingoCardIds.length, function(i) {
+      console.log(i);
+      $('#missing' + i).find('img').attr('src', 'images/' + allBingoCardIds[i] + '.svg');
+    });
+  }
+
+  function bindEventHandler() {
+    $('#neueKarte').click(function() {
+      initBingoCard();
+    });
+
+    $('#klein').click(function() {
+      resizeTiles(75);
+    });
+
+    $('#normal').click(function() {
+      resizeTiles(100);
+    });
+
+    $('#mittel').click(function() {
+      resizeTiles(150);
+    });
+    
+    $('#gross').click(function() {
+      resizeTiles(200);
+    });
+
+    $('#riesig').click(function() {
+      resizeTiles(300);
+    });
+
+    $("#BingoBody td").click(bingoCardClicked);
+  }
+
+  function bingoCardClicked() {
+    $(this).toggleClass("clickedCell");
+  }
+
+  function resizeTiles(tileSize) {
+    $('div.resize img').width(tileSize).height(tileSize);
+    $('div.resize td').width(tileSize).height(tileSize);
+    $('div.resize').width(tileSize * config.bingoCard.width + 50)
+  }
 });
