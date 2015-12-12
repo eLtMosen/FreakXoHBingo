29f0533a234f8e242c85e11ca8d983d12ffe0055
eLtMosen <koennecke@mosushi.com>
2015-12-12 10:50:23 +0100

    merge

--- bingo/js/script.js
+++ bingo/js/script.js
@@ -35,8 +35,15 @@ $(document).ready(function() {
         "Diamant": [3, 9, 11, 15, 19, 21, 27, 29, 33, 37, 39, 45],
         "AT zeichen": [1, 2, 3, 4, 5, 7, 13, 14, 17, 20, 21, 23, 25, 27, 28, 30, 31, 32, 33, 35, 43, 44, 45, 46, 47, 48],
         "Bierkrug": [8, 11, 12, 15, 18, 20, 22, 25, 27, 29, 32, 33, 36, 39, 44, 45],
-        "Ecken aussen": [0, 6, 42, 48],
+        "Ecken mitte": [8, 12, 36, 40],
+	"Ecken aussen": [0, 6, 42, 48],
 	"Zen Garten": [11, 15, 33, 37],
+	"Kreuz": [17, 23, 25, 31],
+	"Briefmarke": [12, 13, 19, 20],
+	"Pong": [7, 11, 14, 34, 41],
+	"Ameisen": [1, 19, 22, 37, 46],
+	"Speed Racer": [8, 12, 29, 33, 38],
+	"BreakOut": [8, 10, 12, 23, 39],
 	"Saphir": [10, 16, 18, 22, 26, 30, 32, 38],
 	"CCC": [1, 2, 7, 14, 18, 19, 21, 27, 29, 30, 33, 41, 46, 47],
 	"Knochen": [15, 19, 22, 23, 24, 25, 26, 29, 33],
@@ -219,11 +226,15 @@ $(document).ready(function() {
       PlayMode = true;
       UserRejectedNum = 0;
       $('#score').html(pad(totalScore, 5));
-      $('#modeindicator').html('<img src="images/spiel_laeuft.svg">');
+      $('#counter2').html('&nbsp');
+      //new CountUp(((new Date()).getTime()+10000), 'counter');
+       $('#modeindicator').html('<img src="images/spiel_laeuft.svg">');
       $("#BuzzwordsBody td").removeClass('missing');
       $("#BuzzwordsBody td").removeClass('rote_zelle');
       $("#BingoBody td").removeClass('missing');
       $("#BingoBody td").removeClass('rote_zelle');
+      $("#BuzzwordsBody td").addClass('mouseover');
+      $("#BingoBody td").addClass('mouseover');
     });    
     
 
@@ -241,10 +252,14 @@ $(document).ready(function() {
       UserRejected = [];
       PlayMode = true;
       $('#score').html(pad(totalScore, 4));
+      $('#counter2').html('&nbsp');
+      //new CountUp(((new Date()).getTime()+10000), 'counter');
       $("#BuzzwordsBody td").removeClass('missing');
       $("#BuzzwordsBody td").removeClass('rote_zelle');
       $("#BingoBody td").removeClass('missing');
       $("#BingoBody td").removeClass('rote_zelle');
+      $("#BuzzwordsBody td").addClass('mouseover');
+      $("#BingoBody td").addClass('mouseover');
       
     }else{
       if (!PlayMode) {
@@ -348,4 +363,71 @@ function pad(n, width, z) {
 //    $('div.resize td').width(tileSize).height(tileSize);
 //    $('div.resize').width(tileSize * config.bingoCard.width + 50)
 //  }
+
+
+/**********************************************************************************************
+* CountUp script by Praveen Lobo (http://PraveenLobo.com/techblog/javascript-countup-timer/)
+* This notice MUST stay intact(in both JS file and SCRIPT tag) for legal use.
+* http://praveenlobo.com/blog/disclaimer/
+**********************************************************************************************/
+function CountUp(initDate, id){
+    this.beginDate = new Date(initDate);
+    this.countainer = document.getElementById(id);
+    this.numOfDays = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+    this.borrowed = 0, this.years = 0, this.months = 0, this.days = 0;
+    this.hours = 0, this.minutes = 0, this.seconds = 0;
+    this.updateNumOfDays();
+    this.updateCounter();
+}
+  
+CountUp.prototype.updateNumOfDays=function(){
+    var dateNow = new Date();
+    var currYear = dateNow.getFullYear();
+    if ( (currYear % 4 == 0 && currYear % 100 != 0 ) || currYear % 400 == 0 ) {
+        this.numOfDays[1] = 29;
+    }
+    var self = this;
+    setTimeout(function(){self.updateNumOfDays();}, (new Date((currYear+1), 1, 2) - dateNow));
+}
+  
+CountUp.prototype.datePartDiff=function(then, now, MAX){
+    var diff = now - then - this.borrowed;
+    this.borrowed = 0;
+    if ( diff > -1 ) return diff;
+    this.borrowed = 1;
+    return (MAX + diff);
+}
+  
+CountUp.prototype.calculate=function(){
+    var currDate = new Date();
+    var prevDate = this.beginDate;
+    this.seconds = this.datePartDiff(prevDate.getSeconds(), currDate.getSeconds(), 60);
+    this.minutes = this.datePartDiff(prevDate.getMinutes(), currDate.getMinutes(), 60);
+    this.hours = this.datePartDiff(prevDate.getHours(), currDate.getHours(), 24);
+    this.days = this.datePartDiff(prevDate.getDate(), currDate.getDate(), this.numOfDays[currDate.getMonth()]);
+    this.months = this.datePartDiff(prevDate.getMonth(), currDate.getMonth(), 12);
+    this.years = this.datePartDiff(prevDate.getFullYear(), currDate.getFullYear(),0);
+}
+  
+CountUp.prototype.addLeadingZero=function(value){
+    return value < 10 ? ("0" + value) : value;
+}
+  
+CountUp.prototype.formatTime=function(){
+    this.seconds = this.addLeadingZero(this.seconds);
+    this.minutes = this.addLeadingZero(this.minutes);
+    this.hours = this.addLeadingZero(this.hours);
+}
+ 
+CountUp.prototype.updateCounter=function(){
+    this.calculate();
+    this.formatTime();
+    this.countainer.innerHTML =
+        this.hours + '<br>' +
+        this.minutes + '<br>' +
+        this.seconds;
+    var self = this;
+    setTimeout(function(){self.updateCounter();}, 1000);
+}
+
 });
