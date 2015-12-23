dde1ea50b98515787b1ab3964b89fdd8cd9aa60f
bassix <adam@net-i.de>
2015-12-23 21:57:16 +0100

    Script zum Kopieren der Nginx Server Konfiguration angepasst.

--- nginx.sh
+++ nginx.sh
@@ -13,5 +13,6 @@ sudo cp /var/www/FreakXoHBingo/server/nginx.conf /etc/nginx/sites-available/frea
 echo "${underline}${green}The Web-Server configuration copied to target location${reset}"
 sudo ln -s /etc/nginx/sites-available/freakxohbingo.conf /etc/nginx/sites-enabled/freakxohbingo.conf
 echo "${underline}${green}The Web-Server configuration enabled${reset}"
+sudo /etc/init.d/php7.0-fpm restart
 sudo /etc/init.d/nginx restart
 echo "${underline}${green}The Web-Server restarted${reset}"
